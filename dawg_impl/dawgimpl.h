a4c406b05998a03ae46fa03987ea919779dd8133
AstyCo <astyco@hotmail.com>
2017-03-13 01:06:23 +0300

    upd dawg1

--- dawg_impl/dawgimpl.h
+++ dawg_impl/dawgimpl.h
@@ -1,4 +1,34 @@
 #ifndef DAWGIMPL_H
 #define DAWGIMPL_H
 
+#include "nodedawg.h"
+
+<typename TValue>
+class AstyDAWG
+{
+    typedef NodeDAWG<TValue> Node;
+
+    Node _root;
+    QMap<TValue, Node*> _lasts;
+public:
+    AstyDAWG();
+
+    void append(const QList<TValue> &record)
+    {
+        Node *p_node = &_root;
+
+        for (int i=0; i < arraySize; ++i) {
+
+            if (!p_node->childs.contains(array[i])) {
+                p_node->insert(array + i, arraySize - i);
+//                p_node->childs.insert(array[i], makeNodes(array, arraySize, i, p_node));
+            }
+
+        }
+    }
+
+};
+
+
+
 #endif // DAWGIMPL_H
--- dawg_impl/nodedawg.h
+++ dawg_impl/nodedawg.h
@@ -1,4 +1,63 @@
 #ifndef NODEDAWG_H
 #define NODEDAWG_H
 
+#include <QMultiMap>
+
+<typename TValue>
+struct NodeDAWG
+{
+    TValue value;
+    int weight;
+
+    QMultiMap<TValue, NodeDAWG*> childs;
+    QMultiMap<TValue, NodeDAWG*> parents;
+
+public:
+    NodeDAWG(const TValue &val,NodeDAWG *par = NULL) : value(val), weight(1)
+    {
+        addParent(par);
+    }
+    ~NodeDAWG()
+    {
+        foreach (NodeDAWG *child, childs){
+            if (!--child->_linkCnt)
+                delete child;
+        }
+    }
+
+    void addChild(NodeDAWG *child)
+    {
+        if (child) {
+            childs.insert(child->value, child);
+            child->addParent(this);
+        }
+    }
+    void removeChild(NodeDAWG *child)
+    {
+        if (child) {
+            childs.remove(child->value, child);
+            child->removeParent(this);
+        }
+    }
+
+
+    void addParent(NodeDAWG *parent)
+    {
+        if (parent) {
+            ++_linkCnt;
+            parents.insert(parent->value, parent);
+        }
+    }
+    void removeParent(NodeDAWG *parent)
+    {
+        if (parent) {
+            --_linkCnt;
+            parents.remove(parent->value, parent);
+        }
+    }
+
+private:
+    int _linkCnt : 16;
+};
+
 #endif // NODEDAWG_H
